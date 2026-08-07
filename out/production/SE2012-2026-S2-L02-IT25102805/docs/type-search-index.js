311b37077e90d5b4c475e32fe1b1528845412535
t25102805 <it25102805@my.sliit.lk>
2026-08-07 06:16:50 +0530

    Remove

--- out/production/SE2012-2026-S2-L02-IT25102805/docs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html","k":"18"},{"p":"models","l":"Course"},{"p":"services","l":"EnrollmentManager"},{"p":"mainapp","l":"Main"},{"p":"models","l":"Student"}];updateSearchResults();
